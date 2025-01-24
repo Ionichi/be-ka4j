23c15f6f1600cb060c425ab34c385c6a5cf25c92
ionichi <feryandi2303@gmail.com>
2025-01-24 16:10:40 +0700

    chore: remove is admin middleware from get and save children attendance endpoint

--- src/routes/absensiChildren.routes.ts
+++ src/routes/absensiChildren.routes.ts
@@ -4,16 +4,14 @@ import {
 } from "domain/absensi-children/controller";
 import express, { Router } from "express";
 import validateAbsensiChildren from "middleware/absensiChildren.middleware";
-import isAdmin from "middleware/admin.middleware";
 import isAuthenticated from "middleware/auth.middleware";
 
 const absensiChildrenRouter: Router = express.Router();
 
-absensiChildrenRouter.get("/", isAuthenticated, isAdmin, getAbsensiChildren);
+absensiChildrenRouter.get("/", isAuthenticated, getAbsensiChildren);
 absensiChildrenRouter.post(
 	"/",
 	isAuthenticated,
-	isAdmin,
 	validateAbsensiChildren,
 	storeAbsensiChildren
 );

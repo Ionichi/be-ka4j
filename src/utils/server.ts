4158c940c33ae4b268d8c13be26dbf45c0f6a09a
ionichi <feryandi2303@gmail.com>
2025-02-03 23:24:31 +0700

    chore: add status server running

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -1,5 +1,5 @@
 import cookieParser from "cookie-parser";
-import express, { json } from "express";
+import express, { json, Request, Response } from "express";
 import router from "routes/index.routes";
 import cors from "cors";
 
@@ -9,5 +9,11 @@ server.use(json());
 server.use(cors({ origin: process.env.FRONTEND_BASE_URL, credentials: true }));
 server.use(cookieParser());
 server.use("/api/v1", router);
+server.get("/", (req: Request, res: Response) => {
+	res.json({
+		status: true,
+		message: "Server is running",
+	});
+});
 
 export default server;
